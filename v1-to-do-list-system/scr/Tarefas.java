16eb9258f52ac5e027ee0707b051725113980f9a
Koehler <bernardoskoehler@gmail.com>
2026-04-06 15:40:48 -0300

    Update Tarefas.java

--- v1-to-do-list-system/scr/Tarefas.java
+++ v1-to-do-list-system/scr/Tarefas.java
@@ -1,7 +1,12 @@
 public class Tarefas {
-	String nome;
+	private String nome;
 	Tarefas(String nome) {
 		this.nome = nome;
+	}
+	private void setNome(String nome) {
+		if(nome != null && !nome.trim().isEmpty()) {
+			this.nome = nome;
+		}
 	}
 	public String getNome() {
 		return nome;
