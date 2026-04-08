d429be692b1cfb700dd3f640d91866a20ddae1d0
Koehler <bernardoskoehler@gmail.com>
2026-04-08 13:39:30 -0300

    Update Produto.java

--- Inventory-System/v2-inventory-system/scr/Produto.java
+++ Inventory-System/v2-inventory-system/scr/Produto.java
@@ -1,1 +1,26 @@
+public class Produto {
+    private String nome;
+    private int quantidade;
 
+    Produto(String nome) {
+        setNome(nome);
+    }
+    private void setNome(String nome) {
+        if(nome != null && !nome.trim().isEmpty()) {
+            this.nome = nome;
+        }
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void adicionarQuantidade(int quantidade) {
+        this.quantidade += quantidade;
+    }
+    public void removerQuantidade(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+}
